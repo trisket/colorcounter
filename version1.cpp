3747bb63591029e17b57a3d2df0d2593031948f3
trisket <tkwatne@gmail.com>
2017-03-05 15:44:07 -0700

    Update version1.cpp

--- version1.cpp
+++ version1.cpp
@@ -6,35 +6,49 @@ using namespace std;
 int main()
 {
 	
-	char yesOrNo; 
-	string color1; 
+	char return1; 
+	string color1;
 	int totalColors,totalDealers, colorQty, total, counter; 
 
 	totalColors = 0;
 	totalDealers = 0; 
 	colorQty = 0;
 	total = 0; 
-	counter = 0;
+	counter = 0; 
+	return1 = 'y';
+	
+	
 
-
-	cout << "Please enter the color you're counting " << endl;
-	cin >> color1; 
-
-	cout << "Please enter the number of dealerships with this color " << endl;
-	cin >> totalDealers;
-
-	while (counter < totalDealers)
+	while (return1 != '#')
 	{
-		cout << "please enter the qty of the dealer and hit enter " << endl;
-		cin >> colorQty;
-		total = total + colorQty;
-		colorQty = 0; 
-		counter++; 
+		cout << "Please enter the name of the color you're counting and press enter " << endl;
+		cin >> color1;
+
+
+		cout << "Please enter the number of dealerships with this color " << endl;
+		cin >> totalDealers;
+
+		while (counter < totalDealers)
+		{
+			cout << "please enter the qty of " << color1 << " at this dealer and hit enter " << endl;
+			cin >> colorQty;
+			total = total + colorQty;
+			colorQty = 0;
+			counter++;
+		}
+
+		cout << "The total # of " << color1 << " counted is: " << total << endl;
+		counter = 0;
+		colorQty = 0;
+
+		cout << "Do you have more to calculate? Type y to continue or # to stop program" << endl;
+		cin >> return1;
+		
+		
 	}
 
-	cout << "The total # of " << color1 << " counted is: " << total << endl; 
+	
 
-	system("pause");
 
 	return 0; 
 
